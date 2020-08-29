feba1a9fcb58687238f40bfac97cef34c9341509
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2020-08-29 17:07:07 -0300

    conectado com a api do github, apresentando o perfil

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api'
 import { BiSearchAlt } from 'react-icons/bi'
+
 import './styles.css'
 
 
 export default function Home() {
+
+  const [name, setName] = useState('')
+  const [inputUserName, setInputUserName] = useState('')
+  const [userName, setUserName] = useState('')
+  const [followers, setFollowers] = useState(0)
+  const [repo, setRepo] = useState(0)
+  const [imgURL, setimgURL] = useState('')
+
+
+  useEffect(() => {
+    getData("LeonardoVita")
+  }, [])
+
+
+  function handleSearch(e) {
+    e.preventDefault()
+    getData(inputUserName)
+  }
+
+  function getData(inputName) {
+    // api.get(`/users/${inputName}`).then(res => {
+    //   const data = res.data
+    //   console.log(res)
+    //   setName(data.name)
+    //   setUserName(data.login)
+    //   setFollowers(data.followers)
+    //   setRepo(data.public_repos)
+    //   setimgURL(data.avatar_url)
+    // })
+
+    api.get(`/users/${inputName}/repos`).then(res => {
+      const data = res.data
+
+      console.log(res)
+    })
+  }
+
+
+
   return (
     <div>
       <header className="header-container">
@@ -11,29 +52,38 @@ export default function Home() {
         <p>Este app tem como objetivo utilizar um api externa do github para apresentar os profiles e seus respectivos repositórios</p>
       </header>
       <div className="container">
-        <form action="" >
+        <form onSubmit={handleSearch}  >
           <h2 className="text-label"></h2>
-          <div>
-            <input type="text" placeholder="LeonardoVita" />
+          <div className="grid-8 form-container">
+            <input
+              type="text"
+              placeholder="LeonardoVita"
+              value={inputUserName}
+              onChange={e => setInputUserName(e.target.value)}
+            />
             <button type="submit"><BiSearchAlt className="search-icon" size="26" color="#fff" /></button>
           </div>
         </form>
       </div>
       <div className="container profile-container">
-        <img src="" alt="" />
-        <div className="info-container">
-          <p>Nome: Leonardo Vita</p>
-          <p>Username: LeonardoVita</p>
-          <p>Followers: 11</p>
-          <p>Repositorios count: 12</p>
+
+        <div className="grid-4">
+          <img src={imgURL} alt="teste" className="avatar-img" />
         </div>
-        <div className="rep-container">
+        <div className="grid-6 info-container">
+          <p>Nome: {name}</p>
+          <p>Username: {userName}</p>
+          <p>Followers: {followers}</p>
+          <p>Public Repos: {repo}</p>
+        </div>
+        <div className="grid-6 rep-container">
           <h2>Novos repositorios</h2>
           <a href="#">github-profiles</a>
           <a href="#">GitHub-Status-Server</a>
           <a href="#">SemanaOmnistack11</a>
           <a href="#">doe-sangue</a>
         </div>
+
       </div>
     </div>
   )

8f217c9e58cf5a9924aac2f5fdad309abf95e549
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2020-11-03 20:18:23 -0300

    normalizando components para o codacy

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Head(props) {
 
   React.useEffect(()=>{
-    document.title = 'GHP | ' + props.title;
+    document.title = "GHP | " + props.title;
     document
       .querySelector("meta[name='description']")
-      .setAttribute('content', props.description);
-  },[props])
+      .setAttribute("content", props.description);
+  },[props]);
 
   return (
     <></>
-  )
-}
+  );
+};
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { BiPowerOff } from 'react-icons/bi'
+import React from "react";
+import { BiPowerOff } from "react-icons/bi";
 
-import style from './header.module.css'
+import style from "./header.module.css";
 
 export default function Header({setLogoutModal, paragraph}) {
   return (
-    <header className={style['header-container']}>
-      <div className={`container ${style['header-content']}`}> 
-        <div className={style['header-title']}>
+    <header className={style["header-container"]}>
+      <div className={`container ${style["header-content"]}`}> 
+        <div className={style["header-title"]}>
           <h1>GHProfiles</h1>
           <p>{paragraph}</p>
         </div>
         {
           setLogoutModal && (
-          <button className={style['logout-button']} onClick={()=>setLogoutModal(true)}>
+          <button className={style["logout-button"]} onClick={()=>setLogoutModal(true)}>
             <BiPowerOff size="24px"/>
           </button>
           )
         }
       </div>
     </header>
-  )
-}
+  );
+};
